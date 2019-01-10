14765d2b2ee43df9bd73f4b5680e0b432eb0381c
Alex Frs <afrancoi@student.42.fr>
2019-01-10 02:08:05 +0100

    added MAX_TETRI

--- fillit.h
+++ fillit.h
@@ -6,14 +6,15 @@
 /*   By: afrancoi <afrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 17:32:12 by afrancoi          #+#    #+#             */
-/*   Updated: 2018/12/12 06:50:33 by afrancoi         ###   ########.fr       */
+/*   Updated: 2019/01/08 22:04:19 by afrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
 # define FILLIT_H
-
+# define MAX_TETRI 545
 int		check_errors(int argc, char **argv);
-void	read_sample(int fd);
+char	*read_sample(int fd);
+void	check_sample(char *sample);
 void	error_exit(void);
 #endif
