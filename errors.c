e0768352cc97ee31add3693e7ddab7723f3ebcf9
Alex Frs <afrancoi@student.42.fr>
2019-01-30 02:35:58 +0100

    return 1 instead of 0 on error_exit()

--- errors.c
+++ errors.c
@@ -6,7 +6,7 @@
 /*   By: afrancoi <afrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 16:58:32 by afrancoi          #+#    #+#             */
-/*   Updated: 2019/01/29 02:56:42 by afrancoi         ###   ########.fr       */
+/*   Updated: 2019/01/30 02:33:37 by afrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,15 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
+/*
+** Free the sample, put an error to stdin and return 1
+*/
+
 int	error_exit(char *sample)
 {
 	free(sample);
 	ft_putendl("error");
-	return (0);
+	return (1);
 }
 
 /*
